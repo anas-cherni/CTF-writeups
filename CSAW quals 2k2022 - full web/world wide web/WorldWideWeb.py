fe14d5e080fcf322bca513d58efd642428afa187
anascherni <cherniii.anas@gmail.com>
2022-09-13 12:43:12 +0100

    Updated CSAW quals 2022

---
+++ CSAW quals 2k2022 - full web/world wide web/WorldWideWeb.py
@@ -0,0 +1,24 @@
+import requests
+
+url="http://web.chal.csaw.io:5010/"
+
+
+session = requests.Session()
+
+
+with session : 
+    next_word='stuff'
+    while True:
+        response1= session.get(url+next_word)
+        """print(session.cookies)"""
+        try:
+            startingIndex=response1.text.index('href="/')
+            endText=response1.text[startingIndex:].index('">')
+        except :
+            print(response1.text)
+            break
+        print("next word:",response1.text[7+startingIndex:endText+startingIndex])
+        next_word=response1.text[7+startingIndex:endText+startingIndex]
+
+
+
---
+++ CSAW quals 2k2022 - full web/writeup.md
@@ -0,0 +1,9 @@
+# CSAW 2022 - full web writeups
+## INTRO
+
+We,  ***SOter14***, spotted 1st place in ***Middle East and North Africa*** region, and 13th worldwide out of more than 900 participating teams and got invited to attend the finals, onsite in ABU DHABI - Emirates. 
+We managed to solve 27 out of 30 task, and we've got solution for 2 out of the 3 remaining tasks (1 crypto, 1 pwn) working locally but had issues with remote server, hard luck next time!
+
+// insert img of scoreboard and tasks solved
+
+## My Little website (204 solves)
